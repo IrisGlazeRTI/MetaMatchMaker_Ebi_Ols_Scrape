e782176609a6df248205a0c0cb6403ab575131d6
Iris Glaze <kglaze@rti.org>
2022-02-04 11:35:37 -0500

    writing to the output files as requests complete.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncio
 import json
 import pprint
 import time
+import csv
 
 # generate random integer values
 from random import seed
@@ -31,10 +32,12 @@ def request(session, url, identifyingVal):
         if response.status_code != 200:
             print("FAILURE::{0}".format(url))
             data = ""
+        else:
+            print("SUCCESS::{0}".format(identifyingVal))
 
-        elapsed_time = default_timer() - START_TIME
-        completed_at = "{:5.2f}s".format(elapsed_time)
-        print("{0:<30} {1:>20}".format(identifyingVal, completed_at))
+        # elapsed_time = default_timer() - START_TIME
+        # completed_at = "{:5.2f}s".format(elapsed_time)
+        # print("{0:<30} {1:>20}".format(identifyingVal, completed_at))
         return data
 
 # Page numbers 0 - 7148. Size will be max of 1000; will probably make it be 1000 every time.
@@ -53,10 +56,9 @@ async def start_async_process():
     childrenUrlsArray = []
     # pagesArray = [0, 1, 2]
     pagesArray = list(range(0, 4))
-    # pagesArray = list(range(0, 7156))
-    with ThreadPoolExecutor(max_workers=10) as executor:
+    #pagesArray = list(range(0, 7156))
+    with ThreadPoolExecutor(max_workers=100) as executor:
         with requests.Session() as session:
-
             # asynchronously make an api call for each study
             loop = asyncio.get_event_loop()
             START_TIME = default_timer()
@@ -72,13 +74,11 @@ async def start_async_process():
             # Run once everything is complete.
             for response in await asyncio.gather(*tasks):
                 inputJson = response
-                buildTermApiUrlsArrays(inputJson, jsTreeUrlsArray, childrenUrlsArray)
+                with open('js_tree_urls_output.tsv', 'wt') as j_out_file, open('children_urls_output.tsv', 'wt') as c_out_file:
+                    tsv_writer_j = csv.writer(j_out_file, delimiter='\t')
+                    tsv_writer_c = csv.writer(c_out_file, delimiter='\t')
+                    buildTermApiUrlsArrays(inputJson, jsTreeUrlsArray, childrenUrlsArray, tsv_writer_j, tsv_writer_c)
                 pass
-
-            print("jsTreeUrlsArray")
-            print(len(jsTreeUrlsArray))
-            print("childrenUrlsArray")
-            print(len(childrenUrlsArray))
 
 
 def writeAllJsTree(term):
@@ -92,28 +92,32 @@ def writeTextToFile(outputFileName, text):
     return None
 
 
-def buildTermApiUrlsArrays(inputJson, jsTreeUrlsArray, childrenUrlsArray):
+def buildTermApiUrlsArrays(inputJson, jsTreeUrlsArray, childrenUrlsArray, tsvWriterJsTree, tsvWriterChildren):
     inputJsonObj = json.loads(inputJson)
-    if (("_embedded" in inputJsonObj) & ("terms" in inputJsonObj["_embedded"])):
+    if ("_embedded" in inputJsonObj) & ("terms" in inputJsonObj["_embedded"]):
         termsList = inputJsonObj["_embedded"]["terms"]
         for term in termsList:
-            if (("label" in term)):
+            if "label" in term:
                 termName = term["label"]
                 # print(termName)
 
-            if (("_links" in term)):
+            if "_links" in term:
                 # pprint.pprint(term["_links"])
 
-                if (("jstree" in term["_links"])):
-                    if ("href" in term["_links"]["jstree"]):
+                if "jstree" in term["_links"]:
+                    if "href" in term["_links"]["jstree"]:
                         jsTreeUrl = term["_links"]["jstree"]["href"] + "?viewMode=All&siblings=false"
-                        jsTreeUrlsArray.append(jsTreeUrl)
+                        if jsTreeUrl not in jsTreeUrlsArray:
+                            jsTreeUrlsArray.append(jsTreeUrl)
+                            tsvWriterJsTree.writerow([jsTreeUrl])
                 # else:
                 #     keep an eye out for "is_root" and "is_obsolete"
-                if (("children" in term["_links"])):
-                    if ("href" in term["_links"]["children"]):
+                if "children" in term["_links"]:
+                    if "href" in term["_links"]["children"]:
                         childrenUrl = term["_links"]["children"]["href"]
-                        childrenUrlsArray.append(childrenUrl)
+                        if childrenUrl not in childrenUrlsArray:
+                            childrenUrlsArray.append(childrenUrl)
+                            tsvWriterChildren.writerow([childrenUrl])
 
 
 # Press the green button in the gutter to run the script.
